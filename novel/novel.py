40345835f43985059c75fd2496e37c0c0348e665
Nick Murdoch <nick@nivan.net>
2015-11-01 11:01:23 +0000

    Basic world generation

---
+++ novel/novel.py
@@ -0,0 +1,47 @@
+import random
+
+class World:
+    def __init__(self, x, y):
+        self.coord = []
+        self.sizex = x
+        self.sizey = y
+        for i in range(x):
+            row = []
+            self.coord.append(row)
+            for j in range(y):
+                row.append(Tile.from_random())
+    
+    def __str__(self):
+        s = []
+        for x in reversed(range(self.sizex)):
+            for y in range(self.sizey):
+                s.append("%s(%d,%d) | " % (self.coord[x][y], x, y))
+            s.append('\n')
+        return ''.join(s)
+
+
+class Tile:
+    terrains = {
+        '~': 'water',
+        '#': 'forest',
+        '_': 'meadow',
+    }
+
+    def __init__(self, terrain):
+        self.terrain = terrain
+
+    @classmethod
+    def from_random(cls):
+        terrain = random.choice(list(cls.terrains.keys()))
+        return cls(terrain)
+    
+    def __str__(self):
+        return self.terrain
+
+def novel(x, y):
+    world = World(x, y)
+    print(world)
+
+if __name__ == '__main__':
+    import sys
+    novel(int(sys.argv[1]), int(sys.argv[2]))
