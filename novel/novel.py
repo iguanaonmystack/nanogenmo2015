bc4661fe8d07d2061db0553569149aab366325ae
Nick Murdoch <nick@nivan.net>
2015-11-01 17:34:57 +0000

    Generate some people.

--- novel/novel.py
+++ novel/novel.py
@@ -1,4 +1,7 @@
+import csv
 import random
+import bisect
+import itertools
 
 class World:
     def __init__(self, x, y):
@@ -38,9 +41,45 @@ class Tile:
     def __str__(self):
         return self.terrain
 
+class Person:
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+    @classmethod
+    def from_random(cls, namegen):
+        return cls(*namegen())
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.gender)
+
+class NameGenerator:
+    def __init__(self, csvfile):
+        self.choices = []
+        self.weights = []
+        reader = csv.reader(csvfile)
+        for firstname, gender, weighting in reader:
+            self.choices.append((firstname, gender))
+            self.weights.append(int(weighting))
+            self.cumdist = list(itertools.accumulate(self.weights))
+
+    def __call__(self):
+        x = random.random() * self.cumdist[-1]
+        return self.choices[bisect.bisect(self.cumdist, x)]
+
 def novel(x, y):
     world = World(x, y)
+    names = []
+    with open('names.csv') as csvfile:
+        namegen = NameGenerator(csvfile)
+    
+    people = []
+    for i in range(12):
+        people.append(Person.from_random(namegen))
+
     print(world)
+    for person in people:
+        print(person)
 
 if __name__ == '__main__':
     import sys
