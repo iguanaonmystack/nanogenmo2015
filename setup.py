70355519cc4978d7db43fa24a50377df569dc7ab
Nick Murdoch <nick@nivan.net>
2016-09-23 21:48:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Novel',
+      version='1.0',
+      description='Nanogenmo 2015 entry',
+      author='Nick Murdoch',
+      author_email='nick@nivan.net',
+      url='https://github.com/flexo/nanogenmo2015',
+      packages=['novel'],
+     )
